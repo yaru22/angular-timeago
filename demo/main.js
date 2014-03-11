1453981f94d3807baa5ce8a2d836185d2f1e25f9
Brian Park <yaru22@gmail.com>
2014-03-11 01:33:42 -0400

    Added timeAgo directive/filter/service.
    TODOs:
    - Need to organize the code further.
    - Add test cases.
    - Clean up grunt tasks (e.g. copying css not needed).

---
+++ demo/main.js
@@ -0,0 +1,11 @@
+/* globals angular */
+'use strict';
+
+var app = angular.module('ngApp', [
+  'yaru22.angular-timeago'
+]);
+
+app.controller('demoController', function ($scope, nowTime) {
+  $scope.pageLoadTime = (new Date()).toISOString();
+  $scope.nowTime = nowTime;
+});
---
+++ test/unit/timeAgoSpec.js
@@ -0,0 +1,28 @@
+/* globals angular, beforeEach, chai, describe, inject, it */
+'use strict';
+
+var expect = chai.expect;
+
+describe('blink', function () {
+  var elm, scope;
+
+  // load the code
+  beforeEach(module('yaru22.angular-timeago'));
+
+  beforeEach(inject(function ($rootScope, $compile) {
+    // we might move this tpl into an html file as well...
+    elm = angular.element('<blink>Hello world</blink>');
+
+    scope = $rootScope;
+    $compile(elm)(scope);
+    scope.$digest();
+  }));
+
+
+  it('should create a marquee element', inject(function() {
+    var marquee = elm.find('marquee');
+
+    expect(marquee).to.have.length(1);
+    expect(marquee.eq(0).text()).to.equal('Hello world');
+  }));
+});
