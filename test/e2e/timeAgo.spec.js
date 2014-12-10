0831ac18efc10fff6b24efa6322590de3038fec2
Amit Jakubowicz <ajk@voxelbrain.com>
2014-12-10 10:17:05 +0100

    typo on e2e test

--- test/e2e/timeAgo.spec.js
+++ test/e2e/timeAgo.spec.js
@@ -6,7 +6,7 @@ describe('basic frontend testing', function () {
     browser.get('/');
   });
 
-  it('archive first shorturl', function () {
+  it('time ago directive', function () {
     expect(element(by.css('.directive-example')).getText()).toMatch(/^(Directive Example\nYou opened this demo page )(less than|about)( a minute ago)$/);
   });
 
