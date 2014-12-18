a2bf57316e61d563f030bc6768dd084373830673
Brian Park <yaru22@gmail.com>
2014-12-18 11:24:16 -0500

    Bumped up the version to 0.1.6.

--- demo/angular-timeago.css
+++ demo/angular-timeago.css
@@ -1,6 +1,6 @@
 /**
  * Angular directive/filter/service for formatting date so that it displays how long ago the given time was compared to now.
- * @version v0.1.5 - 2014-12-10
+ * @version v0.1.6 - 2014-12-18
  * @link https://github.com/yaru22/angular-timeago
  * @author Brian Park <yaru22@gmail.com>
  * @license MIT License, http://www.opensource.org/licenses/MIT
