7ebbf833528fe56bd0448ba276c642ed738ccaeb
Piotr Plenik <piotr.plenik@gmail.com>
2016-04-20 13:17:59 +0200

    added support for polish language (pl_PL)

--- src/timeAgo.js
+++ src/timeAgo.js
@@ -42,6 +42,24 @@ angular.module('yaru22.angular-timeago', [
     overrideLang : null,
     fullDateAfterSeconds : null,
     strings: {
+      'pl_PL': {
+        prefixAgo: null,
+        prefixFromNow: null,
+        suffixAgo: 'temu',
+        suffixFromNow: 'od teraz',
+        seconds: 'mniej niż minuta',
+        minute: 'około minuty',
+        minutes: '%d minut',
+        hour: 'około godziny',
+        hours: 'około %d godzin',
+        day: 'dzień',
+        days: '%d dni',
+        month: 'około miesiąca',
+        months: '%d miesięcy',
+        year: 'około roku',
+        years: '%d lat',
+        numbers: []
+      },
       'it_IT': {
         prefixAgo: null,
         prefixFromNow: null,
