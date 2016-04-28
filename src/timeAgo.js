3f912034fa8fa93ae12ae15f4c77b1dc25d9fee2
wallace <creever@playhold.com>
2016-04-28 16:50:24 +0200

    hungarian language support

--- src/timeAgo.js
+++ src/timeAgo.js
@@ -251,6 +251,24 @@ angular.module('yaru22.angular-timeago', [
         year: 'cirka ett år',
         years: '%d år',
         numbers: []
+      },
+	  'hu_HU': {
+        prefixAgo: null,
+        prefixFromNow: null,
+        suffixAgo: null,
+        suffixFromNow: null,
+        seconds: 'kevesebb mint egy perce',
+        minute: 'körülbelül egy perce',
+        minutes: '%d perce',
+        hour: 'körülbelül egy órája',
+        hours: 'körülbelül %d órája',
+        day: 'egy napja',
+        days: '%d napja',
+        month: 'körülbelül egy hónapja',
+        months: '%d hónapja',
+        year: 'körülbelül egy éve',
+        years: '%d éve',
+        numbers: []
       }
     }
   };
