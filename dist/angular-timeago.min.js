5dd7e64883c70d12938d79c59991a1b79c391307
Brian Park <yaru22@gmail.com>
2016-04-28 10:21:01 -0400

    chore: Version bump from 0.2.6 -> 0.2.7.

--- dist/angular-timeago.min.js
+++ dist/angular-timeago.min.js
@@ -1,6 +1,6 @@
 /**
  * Angular directive/filter/service for formatting date so that it displays how long ago the given time was compared to now.
- * @version v0.2.6 - 2016-04-26
+ * @version v0.2.7 - 2016-04-27
  * @link https://github.com/yaru22/angular-timeago
  * @author Brian Park <yaru22@gmail.com>
  * @license MIT License, http://www.opensource.org/licenses/MIT
