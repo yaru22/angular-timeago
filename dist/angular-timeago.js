a0fba449d107063e12adaa78dbe2e7cba8ffbed9
Brian Park <yaru22@gmail.com>
2016-05-11 16:29:19 -0400

    fix: JSHint errors.

--- dist/angular-timeago.js
+++ dist/angular-timeago.js
@@ -493,9 +493,11 @@ angular.module('yaru22.angular-timeago').factory('timeAgo', ["$filter", "timeAgo
 'use strict';
 
 angular.module('yaru22.angular-timeago').filter('timeAgo', ["nowTime", "timeAgo", function(nowTime, timeAgo) {
-  return function(value, format, timezone) {
+  function timeAgoFilter(value, format, timezone) {
     var fromTime = timeAgo.parse(value);
     var diff = nowTime() - fromTime;
     return timeAgo.inWords(diff, fromTime, format, timezone);
-  };
+  }
+  timeAgoFilter.$stateful = true;
+  return timeAgoFilter;
 }]);
