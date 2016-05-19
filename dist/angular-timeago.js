731c6d4f88f791e72af175994a05ea6bc9252384
Brian Park <yaru22@gmail.com>
2016-05-19 11:09:07 -0400

    chore: Version bump from 0.4.0 -> 0.4.1
    - Fixed main attribute in package.json and bower.json

--- dist/angular-timeago.js
+++ dist/angular-timeago.js
@@ -1,6 +1,6 @@
 /**
  * Angular directive/filter/service for formatting date so that it displays how long ago the given time was compared to now.
- * @version v0.4.0 - 2016-05-16
+ * @version v0.4.1 - 2016-05-19
  * @link https://github.com/yaru22/angular-timeago
  * @author Brian Park <yaru22@gmail.com>
  * @license MIT License, http://www.opensource.org/licenses/MIT
