b21fcaa3d053ef2f683ec23d18b6dff0355ae360
Fer Uría <fauria@gmail.com>
2016-06-15 16:24:52 +0200

    Adds Spanish (Spain) translation.

---
+++ src/languages/time-ago-language-es_ES.js
@@ -0,0 +1,22 @@
+'use strict';
+
+angular.module('yaru22.angular-timeago').config(function(timeAgoSettings) {
+  timeAgoSettings.strings['es_LA'] = {
+    prefixAgo: 'hace',
+    prefixFromNow: 'en',
+    suffixAgo: null,
+    suffixFromNow: null,
+    seconds: 'menos de un minuto',
+    minute: 'un minuto',
+    minutes: '%d minutos',
+    hour: 'una hora',
+    hours: '%d horas',
+    day: 'un día',
+    days: '%d días',
+    month: 'un mes',
+    months: '%d meses',
+    year: 'un año',
+    years: '%d años',
+    numbers: []
+  };
+});
